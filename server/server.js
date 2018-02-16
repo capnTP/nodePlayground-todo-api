8acdd0eefb0db0b0928eb7ebd843628228afe60c
Light TP <1mpulse.z10n@gmail.com>
2018-02-16 15:36:42 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -26,6 +27,20 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  })
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id))
+    return res.status(404).send({error: 'ID is not valid'});
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) return res.status(404).send('ID not found');
+    res.send({todo})
+  }, (e) => {
+    res.status(400).send();
   })
 });
 

bf34584c7a3f22c55d80a91470fdb9f2c176871a
Light TP <1mpulse.z10n@gmail.com>
2018-02-20 13:55:14 +0700

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -57,6 +58,26 @@ app.delete('/todos/:id', (req, res) => {
   }, (e) => {
     res.status(400).send();
   })
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id))
+    return res.status(404).send({error: 'ID is not valid'});
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) return res.status(404).send({error: 'ID not found'});
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {

ca69f8ffd58eb8fb78a4a1e3bd8a3c4c04972e83
Light TP <1mpulse.z10n@gmail.com>
2018-03-06 16:20:41 +0700

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = (u) => {
   let user = u;
   let access = 'auth';
-  let token = jwt.sign({_id: user._id.toHexString(), access}, '9889xyz')
+  let token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET)
   .toString();
   user.tokens.push({access, token});
   //user.tokens = user.tokens.concat([{access, token}]);
@@ -68,7 +68,7 @@ UserSchema.statics.findByToken = (model, token) => {
   let decoded;
 
   try {
-    decoded = jwt.verify(token, '9889xyz');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject('Unauthorized token...');
   }
